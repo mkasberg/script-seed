8e31f3e35676b6c0fa71cb5fd42985c08b9de5a6
Mike Kasberg <kasberg.mike@gmail.com>
2018-09-25 13:45:13 -0600

    Add a python seed script

---
+++ python_seed.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description='This is a python script seed. Customize it to get started quickly with python.')
+parser.add_argument('type', nargs='?', default='tomato', help='The type of seed.')
+parser.add_argument('-w', '--water', action='store_true', help='Water the plant.')
+# --help is added automatically!
+
+args = parser.parse_args()
+
+print('You planted a {} seed!'.format(args.type))
+if args.water:
+    print('You watered the plant.')
+
