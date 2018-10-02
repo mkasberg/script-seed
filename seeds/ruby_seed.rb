b75758b8ccd8693bb5d0e970bdfab2ee901e2238
Curran Lipsett <lipsettcurran7@gmail.com>
2018-10-01 21:35:10 -0500

    Update ruby script to conform to tests

--- seeds/ruby_seed.rb
+++ seeds/ruby_seed.rb
@@ -1,16 +1,19 @@
 #!/usr/bin/env ruby
 require 'optparse'
 
-options = {}
 optparse = OptionParser.new do |opts|
-    opts.banner = "This is a ruby script seed. Customize it to get started quickly with ruby.\n\nUsage ruby_seed.rb [options]"
+  opts.banner = 'This is a ruby script seed. Customize it to get started '\
+                "quickly with ruby.\n\nUsage ruby_seed.rb [options]\n\n"\
+                'Default usage: You planted a magic seed!'
 
-    opts.on('-h', '--help', 'Display help') do
-        puts opts
-    end
-    opts.on('-t=s', '--type', 'Provide a type of seed') do |seed|
-        puts "You planted a #{seed} seed"
-    end
-    opts.on_tail("\nDefault output: You planted a magic seed!")
-        puts opts
+  opts.on('-h', '--help', 'Display help') do
+    puts opts
+    exit
+  end
+  opts.on('-t=s', '--type', 'Provide a type of seed') do |seed|
+    puts "You planted a #{seed} seed!"
+    exit
+  end
+  opts.on_tail
+    puts "#{opts}"
 end.parse!
