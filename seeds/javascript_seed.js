d12b6e603326c886ab1fe084f204b5cc2b5b951d
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-10-02 09:07:56 +0700

    Add a javascript seed script

---
+++ seeds/javascript_seed.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env nodejs
+
+function print_usage() {
+  console.log(`Usage: javascript_seed.js [options]
+
+This is a javascript script seed. Customize it to get started quickly with javascript.
+
+Options:
+  -h Prints this help message
+  -t  Specify the type of seed.`)
+}
+
+let type = 'tomato'
+const opts = process.argv.slice(2)
+
+switch (opts[0]) {
+  case '-h':
+    print_usage()
+    break
+  case '-t':
+    type = opts[1]
+  default:
+    console.log(`You planted a ${type} seed!`)
+    break
+}
