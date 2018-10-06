73c895e58a0dcb2d25295350686b911fe7f1a9c2
Mike Kasberg <kasberg.mike@gmail.com>
2018-10-06 14:12:51 -0600

    Clean up JS seed a little

--- seeds/javascript_seed.js
+++ seeds/javascript_seed.js
@@ -6,20 +6,22 @@ function print_usage() {
 This is a javascript script seed. Customize it to get started quickly with javascript.
 
 Options:
-  -h Prints this help message
+  -h  Prints this help message
   -t  Specify the type of seed.`)
 }
 
-let type = 'tomato'
-const opts = process.argv.slice(2)
+let type = 'tomato';
+const opts = process.argv.slice(2);
 
 switch (opts[0]) {
   case '-h':
-    print_usage()
-    break
+    print_usage();
+    break;
   case '-t':
-    type = opts[1]
+    type = opts[1];
+    break;
   default:
-    console.log(`You planted a ${type} seed!`)
-    break
+    break;
 }
+
+console.log(`You planted a ${type} seed!`);
