c89ddf309cb4dded0805faf30713be6746c373b1
Rahul Dhangar <rahuldhangar@gmail.com>
2020-10-07 20:18:41 +0530

    Added CSS & JS for enabling Dark Mode

--- main.js
+++ main.js
@@ -1,108 +1,118 @@
 let seedElement;
 const LANGUAGES = {
-  'awk': {
-      file: 'seeds/awk_seed.awk',
-      class: 'awk'
-  },
-  'bash': {
-    file: 'seeds/bash_seed.sh',
-    class: 'bash'
-  },
-  'erlang': {
-    file: 'seeds/erlang_seed.erl',
-    class: 'erlang'
-  },
-  'groovy': {
-    file: 'seeds/groovy_seed.groovy',
-    class: 'groovy'
-  },
-  'javascript': {
-    file: 'seeds/javascript_seed.js',
-    class: 'javascript'
-  },
-  'perl': {
-    file: 'seeds/perl_seed.pl',
-    class: 'perl'
-  },
-  'php': {
-    file: 'seeds/php_seed.php',
-    class: 'php'
-  },
-  'powershell': {
-      file: 'seeds/powershell_seed.ps1',
-      class: 'powershell'
-  },
-  'python': {
-    file: 'seeds/python_seed.py',
-    class: 'python'
-  },
-  'ruby': {
-    file: 'seeds/ruby_seed.rb',
-    class: 'ruby'
-  },
-  'scala': {
-    file: 'seeds/scala_seed.scala',
-    class: 'scala'
-  }
+    'awk': {
+        file: 'seeds/awk_seed.awk',
+        class: 'awk'
+    },
+    'bash': {
+        file: 'seeds/bash_seed.sh',
+        class: 'bash'
+    },
+    'erlang': {
+        file: 'seeds/erlang_seed.erl',
+        class: 'erlang'
+    },
+    'groovy': {
+        file: 'seeds/groovy_seed.groovy',
+        class: 'groovy'
+    },
+    'javascript': {
+        file: 'seeds/javascript_seed.js',
+        class: 'javascript'
+    },
+    'perl': {
+        file: 'seeds/perl_seed.pl',
+        class: 'perl'
+    },
+    'php': {
+        file: 'seeds/php_seed.php',
+        class: 'php'
+    },
+    'powershell': {
+        file: 'seeds/powershell_seed.ps1',
+        class: 'powershell'
+    },
+    'python': {
+        file: 'seeds/python_seed.py',
+        class: 'python'
+    },
+    'ruby': {
+        file: 'seeds/ruby_seed.rb',
+        class: 'ruby'
+    },
+    'scala': {
+        file: 'seeds/scala_seed.scala',
+        class: 'scala'
+    }
 }
 
 function getFileForLanguage(language) {
-  return LANGUAGES[language].file;
+    return LANGUAGES[language].file;
 }
 
 function getClassForLanguage(language) {
-  return LANGUAGES[language].class;
+    return LANGUAGES[language].class;
 }
 
 function onReady() {
-  seedElement = document.getElementById("seedScript");
+    seedElement = document.getElementById("seedScript");
 
-  const langSelect = document.getElementById("langSelect");
-  langSelect.onchange = function() {
-    onChange(langSelect.value);
-  };
+    const langSelect = document.getElementById("langSelect");
+    langSelect.onchange = function() {
+        onChange(langSelect.value);
+    };
 
-  onChange("bash");
+    onChange("bash");
 }
 
 function onChange(language) {
-  let languageFile = getFileForLanguage(language);
-  let languageClass = getClassForLanguage(language);
+    let languageFile = getFileForLanguage(language);
+    let languageClass = getClassForLanguage(language);
 
-  fetch(languageFile)
-    .then(function(response) {
-      response.text().then(function(text) {
-        seedElement.textContent = text;
-        seedElement.className = `${languageClass} hljs`;
-        hljs.highlightBlock(seedElement);
+    fetch(languageFile)
+        .then(function(response) {
+            response.text().then(function(text) {
+                seedElement.textContent = text;
+                seedElement.className = `${languageClass} hljs`;
+                hljs.highlightBlock(seedElement);
 
-        document.getElementById("downloadButton").href = languageFile;
-      });
-    })
-    .catch(function(error) {
-      console.error("Seed script request failed.", JSON.stringify(error));
-    });
+                document.getElementById("downloadButton").href = languageFile;
+            });
+        })
+        .catch(function(error) {
+            console.error("Seed script request failed.", JSON.stringify(error));
+        });
 }
 
 function copyToClipboard() {
-  // https://stackoverflow.com/questions/47931843/javascript-copy-to-clipboard-not-working
+    // https://stackoverflow.com/questions/47931843/javascript-copy-to-clipboard-not-working
 
-  const element = document.getElementById('seedScript')
-  let range, selection, worked;
+    const element = document.getElementById('seedScript')
+    let range, selection, worked;
 
-  if (document.body.createTextRange) {
-    range = document.body.createTextRange();
-    range.moveToElementText(element);
-    range.select();
-  } else if (window.getSelection) {
-    selection = window.getSelection();
-    range = document.createRange();
-    range.selectNodeContents(element);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
+    if (document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
 
-  document.execCommand("copy");
+    document.execCommand("copy");
 }
+
+$(".inner-switch").on("click", function() {
+    if ($("body").hasClass("dark")) {
+        $("body").removeClass("dark");
+        $(".inner-switch").text("OFF");
+    } else {
+        $("body").addClass("dark");
+        $(".inner-switch").text("ON");
+    }
+});
 
 onReady();
