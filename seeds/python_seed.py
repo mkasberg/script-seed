6ed089fcb1279ccd0e5a3abcf84981c288815f79
Mike Kasberg <kasberg.mike@gmail.com>
2020-10-12 21:46:27 -0600

    Support -t in python_seed.py
    Working toward more uniformity for better testing.

--- seeds/python_seed.py
+++ seeds/python_seed.py
@@ -3,7 +3,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='This is a python script seed. Customize it to get started quickly with python.')
-parser.add_argument('type', nargs='?', default='tomato', help='The type of seed.')
+parser.add_argument('-t', '--type', nargs='?', default='tomato', help='The type of seed.')
 parser.add_argument('-w', '--water', action='store_true', help='Water the plant.')
 # --help is added automatically!
 
