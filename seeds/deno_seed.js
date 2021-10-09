5b9798eb454b9c24ba086ca3db15a33abd7c9fa2
Vignesh Raj <53948153+vigneshrajj@users.noreply.github.com>
2021-10-09 20:57:55 +0530

    Updated usage on deno_seed

--- seeds/deno_seed.js
+++ seeds/deno_seed.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env deno
 //deno seed
 
+function print_usage() {
+    console.log(`Usage: deno_seed.js [options]
+
+This is a deno script seed.
+
+Options:
+  -h  Prints this help message
+  -t  Specify the type of seed.`);
+}
+
 Deno.args.forEach((arg) => {
     switch (arg) {
-        case '-sn':
-        case '--seed-name':
-            console.log('deno seed');
+        case '-h':
+        case '--help':
+            print_usage();
             break;
-        case '-n':
-        case '--name':
-            console.log('Vignesh Raj');
-            break;
-        case '-hy':
-        case '--hacktober-year':
-            console.log('2021');
+        case '-t':
+        case '--type':
+            console.log('Deno seed');
             break;
     }
 });
