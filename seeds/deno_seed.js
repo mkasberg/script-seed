592dfd1ebfba65a50cc3dae76e6ca1910150ca46
vigneshrajj <vigneshhrajj@gmail.com>
2021-10-10 08:22:39 +0530

    made the deno seed file executable

--- seeds/deno_seed.js
+++ seeds/deno_seed.js
@@ -4,11 +4,11 @@ import { parse } from 'https://deno.land/std/flags/mod.ts';
 function print_usage() {
     console.log(`Usage: deno_seed.js [options]
 
-This is a deno script seed.
+                This is a deno script seed.
 
-Options:
--h Prints this help message
--t Specify the type of seed.`);
+                Options:
+                -h Prints this help message
+                -t Specify the type of seed.`);
 }
 const Args = parse(Deno.args);
 
