c6a6ed47521865c6c0dbaac9bd701922f0d2d45f
Mike Kasberg <kasberg.mike@gmail.com>
2021-10-10 09:09:10 -0600

    Reformat Deno seed
    * Remove several levels of indentation from the help message output.
    * Use `deno fmt` to format the script according to deno conventions.

--- seeds/deno_seed.js
+++ seeds/deno_seed.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env -S deno run --allow-all
-import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { parse } from "https://deno.land/std/flags/mod.ts";
 
 function print_usage() {
-    console.log(`Usage: deno_seed.js [options]
+  console.log(`Usage: deno_seed.js [options]
 
-                This is a deno script seed.
+This is a deno script seed.
 
-                Options:
-                -h Prints this help message
-                -t Specify the type of seed.`);
+Options:
+-h Prints this help message
+-t Specify the type of seed.`);
 }
 const Args = parse(Deno.args);
 
 if (Args.h) {
-    print_usage();
+  print_usage();
 } else if (Args.t) {
-    console.log(`You planted a ${Args.t} seed`);
+  console.log(`You planted a ${Args.t} seed`);
 } else {
-    console.log(`You planted a seed`);
+  console.log(`You planted a seed`);
 }
