c3550e4110c12184fdfc396c48f0bdd7286dd082
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-10-06 21:33:31 -0400

    change document.exeCommand to clipboard API

--- main.js
+++ main.js
@@ -1,142 +1,143 @@
 let seedElement;
 const LANGUAGES = {
-    'awk': {
-        file: 'seeds/awk_seed.awk',
-        class: 'language-awk'
-    },
-    'bash': {
-        file: 'seeds/bash_seed.sh',
-        class: 'language-bash'
-    },
-    'basic': {
-        file: 'seeds/basic_seed.bas',
-        class: 'language-basic'
-    },
-    'deno': {
-        file: 'seeds/deno_seed.js',
-        class: 'language-js'
-    },
-    'erlang': {
-        file: 'seeds/erlang_seed.erl',
-        class: 'language-erlang'
-    },
-    'golang': {
-        file: 'seeds/go_seed.go',
-        class: 'language-go'
-    },
-    'groovy': {
-        file: 'seeds/groovy_seed.groovy',
-        class: 'language-groovy'
-    },
-    'javascript': {
-        file: 'seeds/javascript_seed.js',
-        class: 'language-js'
-    },
-	'julia': {
-        file: 'seeds/julia_seed.jl',
-        class: 'language-julia'
-    },
-	'lua': {
-        file: 'seeds/lua_seed.lua',
-        class: 'language-lua'
-    },
-    'perl': {
-        file: 'seeds/perl_seed.pl',
-        class: 'language-perl'
-    },
-    'php': {
-        file: 'seeds/php_seed.php',
-        class: 'language-php'
-    },
-    'powershell': {
-        file: 'seeds/powershell_seed.ps1',
-        class: 'language-powershell'
-    },
-    'python': {
-        file: 'seeds/python_seed.py',
-        class: 'language-python'
-    },
-    'ruby': {
-        file: 'seeds/ruby_seed.rb',
-        class: 'language-ruby'
-    },
-    'scala': {
-        file: 'seeds/scala_seed.scala',
-        class: 'language-scala'
-    },
-	'tcl': {
-        file: 'seeds/tcl_seed.tcl',
-        class: 'language-tcl'
-    }
-}
+  awk: {
+    file: "seeds/awk_seed.awk",
+    class: "language-awk",
+  },
+  bash: {
+    file: "seeds/bash_seed.sh",
+    class: "language-bash",
+  },
+  basic: {
+    file: "seeds/basic_seed.bas",
+    class: "language-basic",
+  },
+  deno: {
+    file: "seeds/deno_seed.js",
+    class: "language-js",
+  },
+  erlang: {
+    file: "seeds/erlang_seed.erl",
+    class: "language-erlang",
+  },
+  golang: {
+    file: "seeds/go_seed.go",
+    class: "language-go",
+  },
+  groovy: {
+    file: "seeds/groovy_seed.groovy",
+    class: "language-groovy",
+  },
+  javascript: {
+    file: "seeds/javascript_seed.js",
+    class: "language-js",
+  },
+  julia: {
+    file: "seeds/julia_seed.jl",
+    class: "language-julia",
+  },
+  lua: {
+    file: "seeds/lua_seed.lua",
+    class: "language-lua",
+  },
+  perl: {
+    file: "seeds/perl_seed.pl",
+    class: "language-perl",
+  },
+  php: {
+    file: "seeds/php_seed.php",
+    class: "language-php",
+  },
+  powershell: {
+    file: "seeds/powershell_seed.ps1",
+    class: "language-powershell",
+  },
+  python: {
+    file: "seeds/python_seed.py",
+    class: "language-python",
+  },
+  ruby: {
+    file: "seeds/ruby_seed.rb",
+    class: "language-ruby",
+  },
+  scala: {
+    file: "seeds/scala_seed.scala",
+    class: "language-scala",
+  },
+  tcl: {
+    file: "seeds/tcl_seed.tcl",
+    class: "language-tcl",
+  },
+};
 
 function getFileForLanguage(language) {
-    return LANGUAGES[language].file;
+  return LANGUAGES[language].file;
 }
 
 function getClassForLanguage(language) {
-    return LANGUAGES[language].class;
+  return LANGUAGES[language].class;
 }
 
 function onReady() {
-    seedElement = document.getElementById("seedScript");
+  seedElement = document.getElementById("seedScript");
 
-    const langSelect = document.getElementById("langSelect");
-    langSelect.onchange = function() {
-        onChange(langSelect.value);
-    };
+  const langSelect = document.getElementById("langSelect");
+  langSelect.onchange = function () {
+    onChange(langSelect.value);
+  };
 
-    onChange("bash");
+  onChange("bash");
 }
 
 function onChange(language) {
-    let languageFile = getFileForLanguage(language);
-    let languageClass = getClassForLanguage(language);
+  let languageFile = getFileForLanguage(language);
+  let languageClass = getClassForLanguage(language);
 
-    fetch(languageFile)
-        .then(function(response) {
-            response.text().then(function(text) {
-                seedElement.textContent = text;
-                seedElement.className = `${languageClass}`;
-                Prism.highlightElement(seedElement);
+  fetch(languageFile)
+    .then(function (response) {
+      response.text().then(function (text) {
+        seedElement.textContent = text;
+        seedElement.className = `${languageClass}`;
+        Prism.highlightElement(seedElement);
 
-                document.getElementById("downloadButton").href = languageFile;
-            });
-        })
-        .catch(function(error) {
-            console.error("Seed script request failed.", JSON.stringify(error));
-        });
+        document.getElementById("downloadButton").href = languageFile;
+      });
+    })
+    .catch(function (error) {
+      console.error("Seed script request failed.", JSON.stringify(error));
+    });
 }
 
 function copyToClipboard() {
-    // https://stackoverflow.com/questions/47931843/javascript-copy-to-clipboard-not-working
+  // https://stackoverflow.com/questions/47931843/javascript-copy-to-clipboard-not-working
 
-    const element = document.getElementById('seedScript')
-    let range, selection, worked;
+  const element = document.getElementById("seedScript");
+  let range, selection, worked;
+  if (document.body.createTextRange) {
+    range = document.body.createTextRange();
+    range.moveToElementText(element);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();
+    range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 
-    if (document.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(element);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents(element);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-
-    document.execCommand("copy");
+  navigator.clipboard.writeText(element.innerText);
 }
 
-document.getElementById("darkModeSwitch").onclick = function(el) {
-    if (el.target.checked) {
-        document.body.className = "dark";
-        document.getElementById("codeStyle").href = "prismjs/styles/prism-one-dark.css";
-    } else {
-        document.body.className = "";
-        document.getElementById("codeStyle").href = "prismjs/styles/prism-one-light.css";
-    }
-}
+document.getElementById("darkModeSwitch").onclick = function (el) {
+  if (el.target.checked) {
+    document.body.className = "dark";
+    document.getElementById("codeStyle").href =
+      "prismjs/styles/prism-one-dark.css";
+  } else {
+    document.body.className = "";
+    document.getElementById("codeStyle").href =
+      "prismjs/styles/prism-one-light.css";
+  }
+};
 
 onReady();
